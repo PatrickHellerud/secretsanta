2f0703bdf0ab577aaca75077d633980ef7cc93f6
PatrickHellerud <63204678+PatrickHellerud@users.noreply.github.com>
2024-05-18 21:57:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,18 @@ from tkinter import *
 from tkinter import ttk
 
 count = 0
-
+participants = ['Example', 'Example2']
 
 def choose_pairs(lst):
     results = {}
     receive_arr = lst.copy()
 
     for person in lst:
-        # hvis siste bare har seg selv igjen å trekke så kjører vi på nytt rekursivt
+        # hvis siste bare har seg selv igjen å trekke så kjører vi på nytt
         if receive_arr == [person]:
             return choose_pairs(lst)
         pick = receive_arr.pop(random.randrange(len(receive_arr)))
-        # while pick ikke er seg selv
         if pick is person:
-            # velg ny og legg tilbake seg selv
             pick = receive_arr.pop()
             receive_arr.append(person)
         results.update({person: pick})
@@ -25,7 +23,7 @@ def choose_pairs(lst):
 
 def show_next():
     global count
-    text1.set("Neste person: " + cont[count])
+    text1.set("Neste person: " + participants[count])
     text2.set("Venter på neste person")
     text3.set("")
     revealbutton.configure(state=NORMAL)
@@ -34,28 +32,23 @@ def show_next():
 
 def reveal():
     global count
-    text2.set("Du fikk: " + pairs[cont[count]])
-    f = open(cont[count] + ".txt", "w")
-    f.write(pairs[cont[count]])
+    text2.set("Du fikk: " + pairs[participants[count]])
+    f = open(participants[count] + ".txt", "w")
+    f.write(pairs[participants[count]])
     f.close()
     revealbutton.configure(state=DISABLED)
     nextbutton.configure(state=NORMAL)
-    text1.set(cont[count])
-    if count == len(cont) - 1:
+    text1.set(participants[count])
+    if count == len(participants) - 1:
         quitbutton = Button(frm, text="TRYKK HER HVIS DU SER DETTE", command=root.quit, font=("Calibri", 45))
         quitbutton.grid(column=0, row=0)
         nextbutton.configure(state=DISABLED)
     count += 1
 
 
-cont = ['Example', 'Example2']
-custom_messages = {
-    'Example': '',
-    'Example2': ''
-}
-print("input = ", cont)
+print("input = ", participants)
 
-pairs = choose_pairs(cont)
+pairs = choose_pairs(participants)
 print("output = ", pairs)
 root = Tk()
 root.title("Secret Santa")
@@ -64,7 +57,7 @@ frm.grid()
 text1 = StringVar()
 text2 = StringVar()
 text3 = StringVar()
-textbox1,  = Label(frm, textvariable=text1, font=("Calibri", 25))
+textbox1 = Label(frm, textvariable=text1, font=("Calibri", 25))
 textbox1.grid(column=1, row=0)
 textbox2 = Label(frm, textvariable=text2, font=("Calibri", 25))
 textbox2.grid(column=1, row=1)
